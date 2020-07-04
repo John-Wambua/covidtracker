e9ab92103d14d120a524bef30798ef52b18acf87
John-Wambua <jwambua307@gmail.com>
2020-07-04 17:00:36 +0300

    modified pagination

--- controllers/countryController.js
+++ controllers/countryController.js
@@ -58,7 +58,6 @@ exports.allCountries=(req,res,next)=>{
             res.status(200).json({
                 status:"success",
                 page:page,
-                totalPages:totalPages,
                 results:countries.length,
                 data:{
                     countries
